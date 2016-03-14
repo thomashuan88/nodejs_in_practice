f36630ad2026c35be3abd2aba05bb2c2e8319437
thomas <thomashuan88@gmail.com>
2016-03-14 10:29:31 +0800

    chapter 1 - 1

---
+++ countstream.js
@@ -0,0 +1,24 @@
+var writable = require('stream').Writable;
+var util = require('util');
+
+module.exports = CountStream;
+
+util.inherits(CountStream, writable);
+
+function CountStream(matchText, options) {
+	writable.call(this, options);
+	this.count = 0;
+	this.matcher = new RegExp(matchText, 'ig');
+}
+
+CountStream.prototype._write = function(chunk, encoding, cb) {
+	var matches = chunk.toString().match(this.matcher);
+	if (matches) {
+		this.count += matches.length;
+	}
+	cb();
+}
+
+CountStream.prototype.end = function() {
+	this.emit('total', this.count);
+}
