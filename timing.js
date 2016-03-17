1934a77367a99395f9a5f09b064ea888a348fea9
thomas <thomashuan88@gmail.com>
2016-03-17 23:58:56 +0800

    chapter 2-13

---
+++ timing.js
@@ -0,0 +1,13 @@
+function Bomb() {
+	this.message = 'Boom!';
+}
+
+Bomb.prototype.explode = function() {
+	console.log(this.message);
+}
+
+var bomb = new Bomb();
+
+var timeoutId = setTimeout(bomb.explode.bind(bomb), 1000);
+
+// clearTimeout(timeoutId);
