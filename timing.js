311e922c9a02e419ffb1a3d445528618b6a91282
thomas <thomashuan88@gmail.com>
2016-03-18 13:59:22 +0800

    chapter 2-15

--- timing.js
+++ timing.js
@@ -12,16 +12,31 @@ var timeoutId = setTimeout(bomb.explode.bind(bomb), 1000);*/
 
 // clearTimeout(timeoutId);
 
-function tick() {
-	console.log('tick:', Date.now());
+// ---------------------------------------------------------------
+
+// function tick() {
+// 	console.log('tick:', Date.now());
+// }
+
+// function tock() {
+// 	console.log('tock:', Date.now());
+// }
+
+// setInterval(tick, 1000);
+
+// setTimeout(function() {
+// 	setInterval(tock, 1000)
+// }, 500);
+
+// -----------------------------------------------------------------
+
+function monitor() {
+	console.log(process.memoryUsage());
 }
 
-function tock() {
-	console.log('tock:', Date.now());
-}
-
-setInterval(tick, 1000);
+var id = setInterval(monitor, 1000);
+id.unref();
 
 setTimeout(function() {
-	setInterval(tock, 1000)
-}, 500);
+	console.log('Done!');
+}, 5000);
