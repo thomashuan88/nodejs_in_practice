16ad58f3b7d22e8ee4a70d1dcd6ac4fbbb71d193
thomas huan <thomashuan88@gmail.com>
2016-03-26 01:25:56 +0800

    Revert "chapter 2-17"
    This reverts commit c204a46d2440c0f4aa3bda591503a8fdd6826039.

--- buffer.js
+++
@@ -1,6 +0,0 @@
-var fs = require('fs');
-var uri = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACsAAAAo...';
-
-var data = uri.split(',')[1];
-var buf = Buffer(data, 'base64');
-fs.writeFileSync('./secondmonkey.png', buf);
