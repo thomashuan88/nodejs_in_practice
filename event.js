bf7e6c571423ff99b00ef6ca85c2c2d787fe5185
thomas huan <thomashuan88@gmail.com>
2016-03-26 22:20:42 +0800

    chapter 4-2

--- event.js
+++ event.js
@@ -1,8 +1,36 @@
 var util = require('util');
 var events = require('events');
 
+var AudioDevice = {
+	play: function(track) {
+		// stub: trigger playback through itunes, mpg123, etc.
+	},
+	stop: function() {
+
+	}
+}
+
 function MusicPlayer() {
+	this.playing = false;
 	events.EventEmitter.call(this);
 }
 
 util.inherits(MusicPlayer, events.EventEmitter);
+
+var musicPlayer = new MusicPlayer();
+
+musicPlayer.on('play', function(track) {
+	this.playing = true;
+	AudioDevice.play(track);
+});
+
+musicPlayer.on('stop', function() {
+	this.playing = false;
+	AudioDevice.stop();
+});
+
+musicPlayer.emit('play', 'The Roots - The Fire');
+
+setTimeout(function() {
+	musicPlayer.emit('stop');
+}, 1000);
