6c4a48f6d5ceed7761d09b30208b22f1b2362da5
thomas huan <thomashuan88@gmail.com>
2016-04-07 00:10:49 +0800

    chapter 5-2

---
+++ stream2.js
@@ -0,0 +1,30 @@
+var stream = require('stream');
+var util = require('util');
+var express = require('express');
+var app = express();
+
+util.inherits(StatStream, stream.Readable);
+
+function StatStream(limit) {
+    stream.Readable.call(this);
+    this.limit = limit;
+}
+
+StatStream.prototype._read = function(size) {
+    console.log(this);
+    if (this.limit == 0) {
+        // done
+        this.push();
+    } else {
+        this.push(util.inspect(process.memoryUsage()));
+        this.push('n');
+        this.limit--;
+    }
+};
+
+app.get('/', function(req, res) {
+    var statStream = new StatStream(10);
+    statStream.pipe(res);
+});
+
+app.listen(3000);
