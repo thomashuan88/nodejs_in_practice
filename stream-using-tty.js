35cc7cf077b3fea33ddb73cc9f5035911a3334e7
thomas huan <thomashuan88@gmail.com>
2016-07-29 10:47:35 +0800

    chapter 5-7

---
+++ stream-using-tty.js
@@ -0,0 +1,35 @@
+var stream = require('stream');
+var util = require('util');
+
+util.inherits(MemoryStream, stream.Readable);
+util.inherits(OutputStream, stream.Writable);
+
+function MemoryStream() {
+    this.isTTY = process.stdout.isTTY;
+    stream.Readable.call(this);
+}
+
+MemoryStream.prototype._read = function() {
+    var text = JSON.stringify(process.memoryUsage()) + '\n';
+    if (this.isTTY) {
+        this.push('\u001b[32m' + text + '\u001b[39m');
+    } else {
+        this.push(text);
+    }
+};
+
+function OutputStream() {
+    stream.Writable.call(this);
+    this.on('pipe', function(dest) {
+        dest.isTTY = this.isTTY;
+    }.bind(this));
+}
+
+OutputStream.prototype._write = function(chunk, encoding, cb) {
+    util.print(chunk.toString());
+    cb();
+};
+
+var MemoryStream = new MemoryStream();
+
+memoryStream.pipe(process.stdout);
---
+++ test-stream-csvparser.js
@@ -0,0 +1,22 @@
+var assert = require('assert');
+var fs = require('fs');
+var CSVParser = require('./stream-csvparser');
+
+var parser = new CSVParser();
+var actual = [];
+
+fs.createReadStream(__dirname + '/sample.csv').pipe(parser);
+
+process.on('exit', function(){
+    actual.push(parser.read());
+    actual.push(parser.read());
+    actual.push(parser.read());
+
+    var expected = [
+        { name: 'Alex', location: 'UK', role: 'admin' },
+        { name: 'Sam', location: 'France', role: 'user' },
+        { name: 'John', location: 'Canada', role: 'user' }
+    ];
+
+    assert.deepEqual(expected, actual);
+})
